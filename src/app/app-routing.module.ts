1562910de3df5c1fbb70f314dbe7111fdb8b1637
Chiien <fuchienhsu@hotmail.com>
2018-10-02 19:38:15 -0300

    Feat(app-routing) add first router and redirect to HomeModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { HomeModule } from './home/home.module';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+    // loadChildren: './home/home.module#HomeModule'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
