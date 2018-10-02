6a5b6ead061128007fc323a38449b1682938c41a
Chiien <fuchienhsu@hotmail.com>
2018-10-02 19:44:39 -0300

    Feat(nps) add nps component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NpsComponent } from './nps/nps.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [NpsComponent]
 })
 export class SharedModule { }
