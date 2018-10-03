b4592fb32475e2cfc4be050a32ec2ceb69713fe9
Chiien <fuchienhsu@hotmail.com>
2018-10-02 21:15:12 -0300

    Feat(nps.component) add ratingNumbers array

--- src/app/shared/nps/nps.component.ts
+++ src/app/shared/nps/nps.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NpsComponent implements OnInit {
 
+  public ratingNumbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
   constructor() { }
 
   ngOnInit() {
