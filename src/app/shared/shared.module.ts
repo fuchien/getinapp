15f19cde473bbaeed0149acd8da386e892824c2b
Chiien <fuchienhsu@hotmail.com>
2018-10-02 21:15:53 -0300

    Feat(shared.module) import npsratingnumber component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NpsComponent } from './nps/nps.component';
+import { NpsRatingNumberComponent } from './nps/nps-rating-number/nps-rating-number.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-    NpsComponent
+    NpsComponent,
+    NpsRatingNumberComponent
   ],
   exports: [
     NpsComponent
