72c7bccf87b284e3504d4daef139047baf1ce601
Chiien <fuchienhsu@hotmail.com>
2018-10-03 22:44:58 -0300

    Feat(shared) import components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NpsComponent } from './nps/nps.component';
-import { NpsRatingNumberComponent } from './nps/nps-rating-number/nps-rating-number.component';
+
+// COMPONENTS
+import { NpsComponent } from './components/nps/nps.component';
+import { NpsDialogComponent } from './dialogs/nps-dialog/nps-dialog.component';
+import { NpsRatingNumberComponent } from './components/nps/nps-rating-number/nps-rating-number.component';
+
+// MODULES
+import { AngularMaterialModule } from './../angular-material.module';
 
 @NgModule({
   imports: [
+    AngularMaterialModule,
     CommonModule
   ],
   declarations: [
     NpsComponent,
-    NpsRatingNumberComponent
+    NpsDialogComponent,
+    NpsRatingNumberComponent,
+  ],
+  entryComponents: [
+    NpsDialogComponent
   ],
   exports: [
     NpsComponent
