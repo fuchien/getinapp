dbc543e08c9b6dca7eca1d3adbbbd6743bbb8e88
Chiien <fuchienhsu@hotmail.com>
2018-10-04 22:40:04 -0300

    Feat(environment) add api_url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  api_url: 'https://staging-api.lejour.com.br/portal/api/v2/nps',
   production: false
 };
 
