88544d886a1e9d729cbc6ab41bca1e4b4d43b978
Chiien <fuchienhsu@hotmail.com>
2018-10-04 22:39:03 -0300

    Feat(angular-material) import spinner and snack

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -3,15 +3,21 @@ import { NgModule } from '@angular/core';
 // MODULES
 import {MatButtonModule} from '@angular/material/button';
 import {MatDialogModule} from '@angular/material/dialog';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 @NgModule({
   imports: [
     MatButtonModule,
-    MatDialogModule
+    MatDialogModule,
+    MatProgressSpinnerModule,
+    MatSnackBarModule
   ],
   exports: [
     MatButtonModule,
-    MatDialogModule
+    MatDialogModule,
+    MatProgressSpinnerModule,
+    MatSnackBarModule
   ]
 })
 export class AngularMaterialModule { }
