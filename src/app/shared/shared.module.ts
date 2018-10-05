551876fbf63556b9fb193af14e67e5abf7dec391
Chiien <fuchienhsu@hotmail.com>
2018-10-04 22:40:38 -0300

    Feat(Shared) import services and module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 // COMPONENTS
 import { NpsComponent } from './components/nps/nps.component';
@@ -10,10 +11,15 @@ import { NpsRatingNumberComponent } from './components/nps/nps-rating-number/nps
 // MODULES
 import { AngularMaterialModule } from './../angular-material.module';
 
+// SERVICES
+import { HttpService } from './services/http-service/http.service';
+import { SnackbarService } from './services/snackbar/snackbar.service';
+
 @NgModule({
   imports: [
     AngularMaterialModule,
     CommonModule,
+    HttpClientModule,
     ReactiveFormsModule
   ],
   declarations: [
@@ -26,6 +32,10 @@ import { AngularMaterialModule } from './../angular-material.module';
   ],
   exports: [
     NpsComponent
+  ],
+  providers: [
+    HttpService,
+    SnackbarService
   ]
 })
 export class SharedModule { }
